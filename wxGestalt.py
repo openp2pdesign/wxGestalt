2ccc669c50ad61a3c9d6ec94e687794bb6c83be7
Massimo Menichinelli <info@openp2pdesign.org>
2015-05-21 13:31:26 +0200

    Update menu and main app

--- wxGestalt.py
+++ wxGestalt.py
@@ -4,8 +4,24 @@ import wx
 import wxClass
 
 
+class wxGestaltApp(wxClass.MyFrame1):
+
+    def __init__(self, *args, **kw):
+        super(wxGestaltApp, self).__init__(*args, **kw)
+        self.InitUI()
+
+    def InitUI(self):
+        self.Show()
+
+    def On_Quit( self, event ):
+        self.Close(True)
+
+    def On_ScanSerialPort( self, event ):
+        event.Skip()
+
+
 if __name__ == '__main__':
     ex = wx.App()
-    ex1 = wxClass.MyFrame1(None)
+    ex1 = wxGestaltApp(None)
     ex1.Show()
     ex.MainLoop()
