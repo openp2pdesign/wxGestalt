0b7cdb4b5a6dab5f2983313d745bea84ff302e01
Massimo Menichinelli <info@openp2pdesign.org>
2015-05-22 12:55:11 +0200

    Improve general classes with input from Ilan Ellison Moyer's thesis

--- Machines/wxMachines.py
+++ Machines/wxMachines.py
@@ -25,18 +25,64 @@ from gestalt.publish import rpc	#remote procedure call dispatcher
 
 # Classes
 
-class Axis():
+# A class for each Node / Axis
+class wxNode():
 
     def __init__(self):
         self.linear = True
         self.rotary = False
 
-class Machine():
+
+# A basic class for each Machine
+class wxMachine():
 
     def __init__(self):
         self.axes = []
 
 
+# Solo/Independent Nodes
+# http://pygestalt.org/VMC_IEM.pdf
+# p. 35
+class wxSolo_Independent():
+
+    def __init__(self):
+        pass
+
+
+# Solo/Gestalt Nodes
+# http://pygestalt.org/VMC_IEM.pdf
+# p. 36
+class wxSolo_Gestalt():
+
+    def __init__(self):
+        pass
+
+
+# Networked/Gestalt Nodes
+# http://pygestalt.org/VMC_IEM.pdf
+# p. 36
+class wxNetworked_Gestalt(wxSolo_Gestalt):
+
+    def __init__(self):
+        pass
+
+
+# Managed/Gestalt Nodes
+# http://pygestalt.org/VMC_IEM.pdf
+# p. 37
+class wxManaged_Gestalt():
+
+    def __init__(self):
+        pass
+
+
+# Compound Nodes
+# http://pygestalt.org/VMC_IEM.pdf
+# p. 38
+class wxCompound_Nodes():
+
+    def __init__(self):
+        pass
 
 
 if __name__ == '__main__':
